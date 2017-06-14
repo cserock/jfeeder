2ff6930219749be47c11d03c72bae527e5859a07
cserock <cserock@gmail.com>
2017-06-14 15:19:53 +0900

    cleaned codes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const CMD_INIT = 1000;
 const CMD_SET = 1001;
 const CMD_GET = 1002;
@@ -8,7 +7,9 @@ const ERR_CD_INVALIDATED_PARAM = 1;
 const ERR_CD_DEFAULT = 10;
 
 var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({port: 8081});
+var wss = new WebSocketServer({
+  port: 8081
+});
 
 var firebase = require("firebase");
 
@@ -18,23 +19,31 @@ var uid = "";
 var name = "";
 var email = "";
 
-var initData = {'onMeal':0};
-var returnData = {'err_cd':0,'data':[]};
+var initData = {
+  'onMeal': 0
+};
+var returnData = {
+  'err_cd': 0,
+  'data': []
+};
 
 // Initialize the default app
 firebase.initializeApp(config);
 
-function setReturnData (errorCode, data){
-    return {'err_cd':errorCode,'data':data};
+function setReturnData(errorCode, data) {
+  return {
+    'err_cd': errorCode,
+    'data': data
+  };
 }
 
-function cmdInit(jsonData, ws){
+function cmdInit(jsonData, ws) {
   console.log("cmdInit");
   console.log("jsonData : " + JSON.stringify(jsonData));
   console.log("sid : " + jsonData.sid);
 
   firebase.database().ref(jsonData.sid).once('value').then(function(snapshot) {
-    if(snapshot.val()){
+    if (snapshot.val()) {
       console.log("exist sid");
     } else {
       console.log("no exist sid");
@@ -51,7 +60,7 @@ function cmdInit(jsonData, ws){
   });
 }
 
-function cmdGet(sid, data, ws){
+function cmdGet(sid, data, ws) {
   console.log("cmdGet");
   console.log("sid : " + sid);
   console.log("data : " + JSON.stringify(data));
@@ -59,16 +68,18 @@ function cmdGet(sid, data, ws){
   var ledRef = firebase.database().ref(sid);
   ledRef.on('value', function(snapshot) {
 
-    if(snapshot.val()){
+    if (snapshot.val()) {
       console.log("onMeal : " + snapshot.val().onMeal);
-      var data = {'onMeal':snapshot.val().onMeal};
+      var data = {
+        'onMeal': snapshot.val().onMeal
+      };
       ws.send(JSON.stringify(setReturnData(ERR_CD_SUCCESS, data)));
     }
 
   });
 }
 
-function cmdSet(sid, data, ws){
+function cmdSet(sid, data, ws) {
   console.log("cmdSet");
   console.log("sid : " + sid);
   console.log("data : " + JSON.stringify(data));
@@ -78,33 +89,33 @@ function cmdSet(sid, data, ws){
 }
 
 wss.on('connection', function(ws) {
-    ws.send('connected...');
+  ws.send('connected...');
 
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
-        try {
-          var jsonObj = JSON.parse(message);
-          var sid = jsonObj.sid;
-          var data = jsonObj.data[0];
+  ws.on('message', function(message) {
+    console.log('received: %s', message);
+    try {
+      var jsonObj = JSON.parse(message);
+      var sid = jsonObj.sid;
+      var data = jsonObj.data[0];
 
-          switch(jsonObj.cmd) {
-            case CMD_INIT:
-                cmdInit(sid, data, ws);
-                break;
-            case CMD_GET:
-                cmdGet(sid, data, ws);
-                break;
-            case CMD_SET:
-                cmdSet(sid, data, ws);
-                break;
-            default:
-                break;
-              }
-        } catch (e) {
-          console.log(e);
-          ws.send(JSON.stringify(setReturnData(ERR_CD_INVALIDATED_PARAM, null)));
-        }
+      switch (jsonObj.cmd) {
+        case CMD_INIT:
+          cmdInit(sid, data, ws);
+          break;
+        case CMD_GET:
+          cmdGet(sid, data, ws);
+          break;
+        case CMD_SET:
+          cmdSet(sid, data, ws);
+          break;
+        default:
+          break;
+      }
+    } catch (e) {
+      console.log(e);
+      ws.send(JSON.stringify(setReturnData(ERR_CD_INVALIDATED_PARAM, null)));
+    }
 
-    });
+  });
 
 });
